2655501b377873471f9c571b22db683e8943e863
Joshua Opsima <josh@PHCEB-L00010.applusglobal.com>
2026-04-14 02:55:01 +0800

    restructuring

--- apps/tarag_app/constants/Colors.ts
+++ apps/tarag_app/constants/Colors.ts
@@ -1,12 +1,12 @@
 export const Colors = {
   light: {
     text: '#2C3333',
-    background: '#F4F4F4',
+    background: '#F5F5F5',
     icon: '#687076', 
 
     primary: '#fff',
-    secondary: '#0065F8',
-    accent: '#00CAFF',
+    secondary: '#FF6500',
+    accent: '#FFC81E',
   },
   dark: {
     text: '#ECEDEE',
--- backend/src/modules/veehive/veehive.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-// import taragRouter from './modules/tarag/tarag.routes';
-
-const router = express.Router();
-
-// router.post('/google', googleAuth);
-
-export default router;
